b1a6f00bd47f1e6b0f228eed9400f22a4902d36f
James <jamesogordon0@gmail.com>
2021-04-17 18:35:32 -0500

    Updated Files

--- resources/js/script.js
+++
@@ -1,5 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiamdvcmRvbjg4IiwiYSI6ImNrbm1henNqOTBvejcyb21vcjRxYm9zZWwifQ.ESrdn-WL_FR6BQSEcDokUg';
-    var map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11'
-    });
