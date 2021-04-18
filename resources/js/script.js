e7af47966fe6578ee68dbedbabe79073404f245a
James <jamesogordon0@gmail.com>
2021-04-17 19:46:36 -0500

    Updated Default Coordinates

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,30 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiamdvcmRvbjg4IiwiYSI6ImNrbm1henNqOTBvejcyb21vcjRxYm9zZWwifQ.ESrdn-WL_FR6BQSEcDokUg';
-    var map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11'
-    });
+const MAPBOX_ACCESS_TOKEN ='pk.eyJ1IjoiamdvcmRvbjg4IiwiYSI6ImNrbm1henNqOTBvejcyb21vcjRxYm9zZWwifQ.ESrdn-WL_FR6BQSEcDokUg' 
+
+var map = new mapboxgl.Map({
+  accessToken: MAPBOX_ACCESS_TOKEN,
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11"
+  })
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true
+})
+
+function setupMap(centerPosition){
+  const map = new mapboxgl.Map({
+    accessToken: MAPBOX_ACCESS_TOKEN,
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: centerPosition,
+    zoom: 15
+  })
+}
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude])
+  console.log(position)
+}
+
+function errorLocation() {
+  setupMap([38.90, 77.03])
+}
